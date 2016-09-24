7f9d191806b4b220bcac8bc4255652c86303c54f
AlexE <zed-chi@mail.ru>
2016-09-24 13:26:26 +0300

    second

--- app.jsx
+++ app.jsx
@@ -1,65 +1,71 @@
-var PLAYERS = [
-  {
-    name: "Jim Hoskins",
-    score: 31,
-    id: 1,
-  },
-  {
-    name: "Andrew Chalkley",
-    score: 35,
-    id: 2,
-  },
-  {
-    name: "Alena Holligan",
-    score: 42,
-    id: 3,
-  },
+var playersArray = [
+    {
+       name:"Jim",
+       score:89,
+       id:1
+    },
+    {
+       name:"Jjkhkjim",
+       score:59,
+       id:2
+    },
+    {
+       name:"pouim",
+       score:60,
+       id:3
+    }
 ];
+//***Header***
+    function Header(props) {
+        return (
+            <div className="header">
+                <h1>{props.title}</h1>
+            </div>
+            );
+    };
 
-function Header(props) {
-  return (
-    <div className="header">
-      <h1>{props.title}</h1>
-    </div>
-  );
+    Header.propTypes = {
+        title: React.PropTypes.string.isRequired
+    };    
+//***End of Header***/    
+
+//***Counter***
+function Counter(props){
+    return (
+        <div className="counter">
+            <button className="counter-action decrement"> - </button>
+            <div className="counter-score"> {props.score} </div>
+            <button className="counter-action increment"> + </button>
+        </div>
+        );
 }
-
-Header.propTypes = {
-  title: React.PropTypes.string.isRequired,
-};
-
-function Counter(props) {
-  return (
-    <div className="counter">
-      <button className="counter-action decrement"> - </button>
-      <div className="counter-score"> {props.score} </div>
-      <button className="counter-action increment"> + </button>
-    </div>
-  );
-}
-
 Counter.propTypes = {
-  score: React.PropTypes.number.isRequired,
-};
+        score: React.PropTypes.number.isRequired,
+    };
+//***End of Counter***
 
-function Player(props) {
-  return (
-    <div className="player">
-      <div className="player-name">
-        {props.name}
-      </div>
-      <div className="player-score">
-        <Counter score={props.score} />
-      </div>
-    </div>
-  );
+
+//***Player***
+
+
+function Player(props){
+    return (
+        <div className="player">
+            <div className="player-name">{props.name}</div>
+            <div className="player-score">
+                <Counter score={props.score} />
+            </div>
+        </div>
+        );
 }
 
 Player.propTypes = {
-  name: React.PropTypes.string.isRequired,
-  score: React.PropTypes.number.isRequired,
-};
+        name: React.PropTypes.string.isRequired
+    };
+//***End of Player***/
 
+
+//***App**
 function Application(props) {
   return (
     <div className="scoreboard">
